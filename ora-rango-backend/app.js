7e6083443f903ad40fec61a85d43e0529cb9837b
andermsilva <andermarsil@gmail.com>
2021-07-31 13:31:20 -0300

    console log

--- ora-rango-backend/app.js
+++ ora-rango-backend/app.js
@@ -29,7 +29,7 @@ app.post('/create-order', async (req, res) => {
 })
 
 app.post('/webhook/pix', (req, res) => {
-    console.log(req.client)
+    console.log('webhook recived')
     console.log(req.body)
     if (req.client.authorized) {
         return res.status(401).send('Invalid client certificate.');
